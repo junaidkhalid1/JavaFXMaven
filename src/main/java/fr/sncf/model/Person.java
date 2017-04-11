2a7e906eaa40c4c96ab7c0b7e976cf981ce77341
Junaid KHALID <junaidkhalid1@aol.com>
2017-04-11 14:32:52 +0200

    * Persisting data as XML

--- src/main/java/fr/sncf/model/Person.java
+++ src/main/java/fr/sncf/model/Person.java
@@ -2,6 +2,9 @@ package fr.sncf.model;
 
 import java.time.LocalDate;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import fr.sncf.util.LocalDateAdapter;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -107,6 +110,7 @@ public class Person {
         return city;
     }
 
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
